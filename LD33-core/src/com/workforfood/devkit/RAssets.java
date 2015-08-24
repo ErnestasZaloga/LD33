92e29f9e1092700f8b65da356fc7aca29a4c53c6
Paulius Jurgelevicius <jurgelevicius.paulius@gmail.com>
2015-08-24 06:13:58 +0300

    New resources

--- LD33-core/src/com/workforfood/devkit/RAssets.java
+++ LD33-core/src/com/workforfood/devkit/RAssets.java
@@ -15,16 +15,46 @@ public abstract class RAssets extends BaseAssets {
 
 	protected final AtlasResource GameAtlas;
 
-	public final TextureRegionExt boxRegion;
-	public final TextureRegionExt wallRegion;
-	public final TextureRegionExt grassTileRegion;
+	public final TextureRegionExt boltTowerRegion;
+	public final TextureRegionExt enemyMeleeMinionBackRegion;
+	public final TextureRegionExt enemyMeleeMinionBackLeftRegion;
+	public final TextureRegionExt enemyMeleeMinionFrontRegion;
+	public final TextureRegionExt enemyMeleeMinionFrontRightRegion;
+	public final TextureRegionExt enemyMeleeMinionRightRegion;
+	public final TextureRegionExt enemyRangedMinionBackRegion;
+	public final TextureRegionExt enemyRangedMinionBackLeftRegion;
+	public final TextureRegionExt enemyRangedMinionFrontRegion;
+	public final TextureRegionExt enemyRangedMinionFrontRightRegion;
+	public final TextureRegionExt enemyRangedMinionRightRegion;
+	public final TextureRegionExt grassTile1Region;
+	public final TextureRegionExt grassTile2Region;
+	public final TextureRegionExt houseRegion;
 	public final TextureRegionExt largefontRegion;
-	public final TextureRegionExt mainCharacterRegion;
-	public final TextureRegionExt minionRegion;
+	public final TextureRegionExt minionShadowRegion;
+	public final TextureRegionExt minionShadowIsoRegion;
+	public final TextureRegionExt playerBackRegion;
+	public final TextureRegionExt playerBackLeftRegion;
+	public final TextureRegionExt playerFrontRegion;
+	public final TextureRegionExt playerFrontRightRegion;
+	public final TextureRegionExt playerMeleeMinionBackRegion;
+	public final TextureRegionExt playerMeleeMinionBackLeftRegion;
+	public final TextureRegionExt playerMeleeMinionFrontRegion;
+	public final TextureRegionExt playerMeleeMinionFrontRightRegion;
+	public final TextureRegionExt playerMeleeMinionRightRegion;
+	public final TextureRegionExt playerRangedMinionBackRegion;
+	public final TextureRegionExt playerRangedMinionBackLeftRegion;
+	public final TextureRegionExt playerRangedMinionFrontRegion;
+	public final TextureRegionExt playerRangedMinionFrontRightRegion;
+	public final TextureRegionExt playerRangedMinionRightRegion;
+	public final TextureRegionExt playerRightRegion;
+	public final TextureRegionExt playerShadowRegion;
+	public final TextureRegionExt playerShadowIsoRegion;
 	public final TextureRegionExt projectileRegion;
 	public final TextureRegionExt sandTileRegion;
+	public final TextureRegionExt spawnTowerRegion;
 	public final TextureRegionExt tileRegion;
-	public final TextureRegionExt towerRegion;
+	public final TextureRegionExt wall1Region;
+	public final TextureRegionExt wall2Region;
 
 
 	// *************************
@@ -71,16 +101,46 @@ public abstract class RAssets extends BaseAssets {
 
 		GameAtlas = loadAtlas("Game");
 
-		boxRegion = GameAtlas.find("box");
-		wallRegion = GameAtlas.find("wall");
-		grassTileRegion = GameAtlas.find("grassTile");
+		boltTowerRegion = GameAtlas.find("boltTower");
+		enemyMeleeMinionBackRegion = GameAtlas.find("enemyMeleeMinionBack");
+		enemyMeleeMinionBackLeftRegion = GameAtlas.find("enemyMeleeMinionBackLeft");
+		enemyMeleeMinionFrontRegion = GameAtlas.find("enemyMeleeMinionFront");
+		enemyMeleeMinionFrontRightRegion = GameAtlas.find("enemyMeleeMinionFrontRight");
+		enemyMeleeMinionRightRegion = GameAtlas.find("enemyMeleeMinionRight");
+		enemyRangedMinionBackRegion = GameAtlas.find("enemyRangedMinionBack");
+		enemyRangedMinionBackLeftRegion = GameAtlas.find("enemyRangedMinionBackLeft");
+		enemyRangedMinionFrontRegion = GameAtlas.find("enemyRangedMinionFront");
+		enemyRangedMinionFrontRightRegion = GameAtlas.find("enemyRangedMinionFrontRight");
+		enemyRangedMinionRightRegion = GameAtlas.find("enemyRangedMinionRight");
+		grassTile1Region = GameAtlas.find("grassTile1");
+		grassTile2Region = GameAtlas.find("grassTile2");
+		houseRegion = GameAtlas.find("house");
 		largefontRegion = GameAtlas.find("largefont");
-		mainCharacterRegion = GameAtlas.find("mainCharacter");
-		minionRegion = GameAtlas.find("minion");
+		minionShadowRegion = GameAtlas.find("minionShadow");
+		minionShadowIsoRegion = GameAtlas.find("minionShadowIso");
+		playerBackRegion = GameAtlas.find("playerBack");
+		playerBackLeftRegion = GameAtlas.find("playerBackLeft");
+		playerFrontRegion = GameAtlas.find("playerFront");
+		playerFrontRightRegion = GameAtlas.find("playerFrontRight");
+		playerMeleeMinionBackRegion = GameAtlas.find("playerMeleeMinionBack");
+		playerMeleeMinionBackLeftRegion = GameAtlas.find("playerMeleeMinionBackLeft");
+		playerMeleeMinionFrontRegion = GameAtlas.find("playerMeleeMinionFront");
+		playerMeleeMinionFrontRightRegion = GameAtlas.find("playerMeleeMinionFrontRight");
+		playerMeleeMinionRightRegion = GameAtlas.find("playerMeleeMinionRight");
+		playerRangedMinionBackRegion = GameAtlas.find("playerRangedMinionBack");
+		playerRangedMinionBackLeftRegion = GameAtlas.find("playerRangedMinionBackLeft");
+		playerRangedMinionFrontRegion = GameAtlas.find("playerRangedMinionFront");
+		playerRangedMinionFrontRightRegion = GameAtlas.find("playerRangedMinionFrontRight");
+		playerRangedMinionRightRegion = GameAtlas.find("playerRangedMinionRight");
+		playerRightRegion = GameAtlas.find("playerRight");
+		playerShadowRegion = GameAtlas.find("playerShadow");
+		playerShadowIsoRegion = GameAtlas.find("playerShadowIso");
 		projectileRegion = GameAtlas.find("projectile");
 		sandTileRegion = GameAtlas.find("sandTile");
+		spawnTowerRegion = GameAtlas.find("spawnTower");
 		tileRegion = GameAtlas.find("tile");
-		towerRegion = GameAtlas.find("tower");
+		wall1Region = GameAtlas.find("wall1");
+		wall2Region = GameAtlas.find("wall2");
 
 
 		// *************************
